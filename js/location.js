d350df266bae629007b5c9f479285d492fc8fb60
Sahan Dilshan <32576163+Amstrom@users.noreply.github.com>
2020-12-30 20:50:09 +0530

    Allow guest to view merchants

--- js/location.js
+++ js/location.js
@@ -2,12 +2,20 @@ $(document).on('click', '.location', function () {
     let location = $(this).data('filtertext');
     console.log(location);
     let user = JSON.parse(sessionStorage.getItem('user'));
-    user['location'] = location
-    sessionStorage.setItem('user', JSON.stringify(user))
+    if (user) {
+        user['location'] = location
+        sessionStorage.setItem('user', JSON.stringify(user))
+    } else {
+        sessionStorage.setItem('location', location);
+    }
     window.location.href = '/HOMEAL/home/Search.html';
 });
 
 $(document).ready(function () {
     let user = JSON.parse(sessionStorage.getItem('user'));
- $('#selectLocation').text(user['location']);
+    if (user) {
+        $('#selectLocation').text(user['location']);
+    } else {
+        $('#selectLocation').text(sessionStorage.getItem('location'));
+    }
 });
